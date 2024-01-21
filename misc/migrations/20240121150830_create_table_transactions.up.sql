ec2f090cb76e3629f712eb7c9b9e7bff14de92ef
osvaldosilitonga <osvaldosilitonga@gmail.com>
2024-01-22 04:15:21 +0700

    Update DB migrations

--- misc/migrations/20240121150830_create_table_transactions.up.sql
+++ misc/migrations/20240121150830_create_table_transactions.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS transactions (
     date BIGINT DEFAULT EXTRACT(epoch FROM NOW()) * 1000,
     type VARCHAR NOT NULL,
     gram FLOAT NOT NULL CHECK (gram >= 0.001),
-    price_id VARCHAR NOT NULL REFERENCES prices(id),
+    price INT NOT NULL,
     balance FLOAT NOT NULL
 );
 
-INSERT INTO transactions(wallet_id, type, gram, price_id, balance)
-VALUES ('w001', 'topup', 0.01, 'p001', 0);
+INSERT INTO transactions(wallet_id, type, gram, price, balance)
+VALUES ('w001', 'topup', 0.01, 90000, 0);
